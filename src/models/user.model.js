aab6f5d71659ecf93d8c8636ddf15f1944e2ae6c
Mukesh Kumar Dalai <mukeshdalai.2307@gmail.com>
2024-01-17 19:26:20 +0530

    defining multer middleware

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -54,6 +54,7 @@ userSchema.pre("save", async function (next){
     this.password = bcrypt.hash(this.password,10)
     next()
 })
+
 userSchema.methods.isPasswordCorrect = async function(password){
     return bcrypt.compare(password,this.password)
 }
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+});
+
+const uploadOnCloud = async (localPath) =>{
+    try {
+        if(!localPath){
+            return null
+        }
+        // uploading file on cloud
+        const response = await cloudinary.uploader.upload(localPath,{
+            resource_type: "auto"
+        })
+        console.log("file uploaded successfully",response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localPath) // remove the locally saved temp file as the upload ops got failed
+    }
+}
+
+export {uploadOnCloud}
